214e15b1463a03cd70abcc26845ef37264286631
tagniam <jrmaingat@gmail.com>
2017-08-08 22:44:54 -0400

    Add setup method

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
 
+import * as YouTubePlayer from 'youtube-player';
+
 @Injectable()
 export class PlayerService {
+  private player: YouTubePlayer;
 
   constructor() { }
+
+  /**
+   * Injects a YouTube iframe into the div with the given id.
+   * @param divId HTML div id reference
+   */
+  setup(divId: string) {
+    this.player = YouTubePlayer(divId);
+  }
 
 }

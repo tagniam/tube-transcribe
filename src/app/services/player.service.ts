457f19a345c265ca4a8facc2801424bccdb8fb66
tagniam <jrmaingat@gmail.com>
2017-08-08 23:01:57 -0400

    Add return types

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -13,13 +13,23 @@ export class PlayerService {
    * Injects a YouTube iframe into the div with the given id.
    * @param divId HTML div id reference
    */
-  setup(divId: string) {
+  setup(divId: string): void {
     this.player = YouTubePlayer(divId);
   }
 
-  loadVideoById(videoId: string) {
+  /**
+   * Loads a YouTube video with the given id.
+   * @param videoId YouTube video id
+   * @param startSeconds starting point of the video in seconds
+   * @param suggestedQuality video playback quality; see YouTube iFrame API
+   */
+  loadVideoById(videoId: string, startSeconds?: number, suggestedQuality?: string): void {
     this.videoId = videoId;
-    this.player.loadVideoById(videoId);
+    this.player.loadVideoById({
+      videoId: videoId,
+      startSeconds: startSeconds,
+      suggestedQuality: suggestedQuality
+    });
   } 
 
 }

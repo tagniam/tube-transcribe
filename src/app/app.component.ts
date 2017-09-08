e85985f74f05b4dd5eea8720c5168525b0c5d734
tagniam <jrmaingat@gmail.com>
2017-09-07 20:17:21 -0400

    Setup placeholder video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlayerService } from './services/player.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  // Placeholder video
+  private videoId = 'br190bnPOLY';
+
+  constructor(private playerService: PlayerService) {
+
+  }
+
+  ngOnInit() {
+    this.playerService.setup('youtube-player', this.videoId);
+  }
+
 }

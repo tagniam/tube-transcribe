0bf1cd522414a292ea136192f6d5946c9df95430
tagniam <jrmaingat@gmail.com>
2017-09-07 20:24:49 -0400

    Move control bar html to its own component

--- src/app/components/control-bar/control-bar.component.ts
+++ src/app/components/control-bar/control-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-control-bar',
+  selector: 'control-bar',
   templateUrl: './control-bar.component.html',
   styleUrls: ['./control-bar.component.css']
 })

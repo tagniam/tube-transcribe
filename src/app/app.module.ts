c07f5cd1c6758d3c4a70039b2291f93294aed7da
tagniam <jrmaingat@gmail.com>
2017-09-07 21:42:14 -0400

    Move components under control-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { PlayerService } from './services/player.service';
 import { TrackMouseDirective } from './directives/track-mouse.directive';
-import { TimelineComponent } from './components/timeline/timeline.component';
-import { SelectionBoxComponent } from './components/timeline/selection-box/selection-box.component';
-import { TimeDisplayComponent } from './components/time-display/time-display.component';
-import { MediaControlsComponent } from './components/media-controls/media-controls.component';
+import { TimelineComponent } from './components/control-bar/timeline/timeline.component';
+import { SelectionBoxComponent } from './components/control-bar/timeline/selection-box/selection-box.component';
+import { TimeDisplayComponent } from './components/control-bar/time-display/time-display.component';
+import { MediaControlsComponent } from './components/control-bar/media-controls/media-controls.component';
 import { ControlBarComponent } from './components/control-bar/control-bar.component';
 
 @NgModule({

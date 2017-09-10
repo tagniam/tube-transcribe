dfefcc84542649a287e1a159ca143e8ba9bab5cb
tagniam <jrmaingat@gmail.com>
2017-09-10 15:46:09 -0400

    Remove old files

--- src/app/directives/track-mouse.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TrackMouseDirective } from './track-mouse.directive';
-
-describe('TrackMouseDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TrackMouseDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/enum/player-states.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum PlayerStates {
-  UNSTARTED = -1,
-  ENDED = 0,
-  PLAYING = 1,
-  PAUSED = 2,
-  BUFFERING = 3,
-  VIDEO_CUED = 5
-}

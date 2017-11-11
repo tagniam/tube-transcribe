b54dbfcba3b5c5025c7603e6b507ce64e8dd7254
J.R. Maingat <jrmaingat@gmail.com>
2017-11-10 19:52:54 -0500

    Add home component

--- client/core/core.module.ts
+++ client/core/core.module.ts
@@ -3,17 +3,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CoreComponent } from './core.component';
+import { HomeComponent } from '../home/home.component';
 import { AppModule, appRoutes } from '../app/app.module';
 
+const coreRoutes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    CoreComponent
+    CoreComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes.concat(coreRoutes))
   ],
   providers: [],
   bootstrap: [CoreComponent]

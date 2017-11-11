328eb1e98d69e3c74090733add80406abac56a24
J.R. Maingat <jrmaingat@gmail.com>
2017-11-10 21:07:10 -0500

    Input and navigation for yt url on homepage

--- client/home/home.component.ts
+++ client/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'home',
@@ -6,8 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor() { }
+  @ViewChild('ytURL') url;
+
+  constructor(private router: Router) { }
 
   ngOnInit() { }
 
+  handleURLSubmit() {
+    let urlText = this.url.nativeElement.value;
+    let params = this.parseQueryString(urlText.substring(urlText.indexOf('?') + 1));
+    if (params['v']) {
+      this.router.navigateByUrl('watch?v=' + params['v']);
+    }
+  }
+
+  parseQueryString(queryString) {
+    var params = {}, queries, temp, i, l;
+    // Split into key/value pairs
+    queries = queryString.split("&");
+    // Convert the array of strings into an object
+    for (i = 0, l = queries.length; i < l; i++) {
+        temp = queries[i].split('=');
+        params[temp[0]] = temp[1];
+    }
+    return params;
+  }
 }

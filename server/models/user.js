8a596c27315701611df08f6cac1febe60ef5c45c
J.R. Maingat <jrmaingat@gmail.com>
2017-11-11 02:27:02 -0500

    Implement login route

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     email: { type: String, required: true },
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const User = require('./models/user');
 
@@ -14,6 +15,36 @@ router.post('/user', (req, res, next) => {
     User.addUser(newUser, (err, user) => {
         if (err) return res.json({ success: false, msg: 'Failed to register user' });
         return res.json({ success: true, msg: 'User registered' });
+    });
+});
+
+// Login
+router.post('/login', (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.getUserByEmail(email, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            return res.json({ success: false, msg: 'User not found' });
+        }
+
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+                // Expires in 1 week
+                const token = jwt.sign(user.toJSON(), 'mysecret', { expiresIn: 604800 });
+                user.password = undefined;
+                return res.json({
+                    success: true,
+                    token: 'JWT '+token,
+                    user: user
+                });
+            }
+
+            else {
+                return res.json({ success: false, msg: 'Wrong password' });
+            }
+        });
     });
 });
 
